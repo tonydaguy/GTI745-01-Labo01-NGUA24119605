fabd546d053d5eb1b626550b68c50a0071a3adab
tonydaguy <anthony6122@hotmail.com>
2022-06-02 04:03:53 -0400

    Adding ShootingPlane for PlaceObjectsOnPlane

--- Assets/UX/Scripts/PlaceObjectsOnPlane.cs
+++ Assets/UX/Scripts/PlaceObjectsOnPlane.cs
@@ -7,6 +7,9 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceObjectsOnPlane : MonoBehaviour
 {
+    public GameObject spawner;
+    public GameObject shootScript;
+
     [SerializeField]
     [Tooltip("Instantiates this prefab on a plane at the touch location.")]
     GameObject m_PlacedPrefab;
@@ -67,8 +70,11 @@ public class PlaceObjectsOnPlane : MonoBehaviour
 
                     if (m_NumberOfPlacedObjects < m_MaxNumberOfObjectsToPlace)
                     {
+                        Spawner.Instance.setTurretPosition(hitPose.position);
+                        Spawner.Instance.spawnRedPlane();
+
                         spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-                        
+                        shootScript.GetComponent<Shoot>().turret = spawnedObject;
                         m_NumberOfPlacedObjects++;
                     }
                     else
